5848687c8538d36daa0a24dc7b161d7502ba1546
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-09-22 15:43:28 -0500

    generate HTML file

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+module.exports = generatePage;
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Porfolio complete! Checkout index.html to see the output!');
+});
